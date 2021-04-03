8d1d7d31e25f0b790d579f26088ad88a6ff04953
Marc <marc63570@gmail.com>
2021-04-03 13:44:54 -0400

    Added allUserData string to use for generateCustomerDataFile

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -17,6 +17,7 @@ class CustomerSystem{
 
         // More variables for the main may be declared in the space below
 
+        String allUserData;
 
         do{
             printMenu();                                    // Printing out the main menu
@@ -25,7 +26,7 @@ class CustomerSystem{
             if (userInput.equals(enterCustomerOption)){
                 // Only the line below may be editted based on the parameter list and how you design the method return
 		        // Any necessary variables may be added to this if section, but nowhere else in the code
-                enterCustomerInfo();
+                allUserData = enterCustomerInfo();
             }
             else if (userInput.equals(generateCustomerOption)) {
                 // Only the line below may be editted based on the parameter list and how you design the method return
@@ -55,7 +56,8 @@ class CustomerSystem{
     * The method may not nesessarily be a void return type
     * This method may also be broken down further depending on your algorithm
     */
-    public static void enterCustomerInfo() {
+    public static String enterCustomerInfo() {
+        return "";
     }
     /*
     * This method may be edited to achieve the task however you like.
